75556aa5b132b19558e58c6e9ae7921e7d9d121f
Karina <lyakhova_karina@list.ru>
2023-04-28 20:14:51 +0300

    5

--- task_5.py
+++ task_5.py
@@ -7,3 +7,14 @@
 Подсказки:
 --- используйте модуль chardet, иначе задание не засчитается!!!
 """
+import subprocess
+import chardet
+
+ping_args = [['ping', 'yandex.ru'], ['ping', 'youtube.com']]
+
+for ping_element in ping_args:
+    ping_process = subprocess.Popen(ping_element, stdout=subprocess.PIPE)
+    for line in ping_process.stdout:
+        res = chardet.detect(line)
+        line = line.decode(res['encoding'])
+        print(line)
