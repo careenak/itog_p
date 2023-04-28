29463a04e50ca0bfb38485e729556d859cfb4486
Karina <lyakhova_karina@list.ru>
2023-04-28 20:13:45 +0300

    4

--- task_4.py
+++ task_4.py
@@ -8,3 +8,15 @@
 Подсказки:
 --- используйте списки и циклы, не дублируйте функции
 """
+string_list = ['разработка', 'администрирование', 'protocol', 'standard']
+
+byte_list = []
+
+for word in string_list:
+    word_bytes = word.encode('utf-8')  # получили байты
+    byte_list.append(word_bytes)
+    print(word_bytes, type(word_bytes))
+
+for byte in byte_list:
+    string = byte.decode('utf-8')  # получили строку
+    print(string, type(string))
